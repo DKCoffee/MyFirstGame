b247024a26c5aa0ec7638d670bd8b246a481ab5b
DKCoffee <amk1295@gmail.com>
2017-11-19 17:00:28 +0100

    Add Enemy movement and shit script

--- Assets/Scripts/EnnemyScript.cs
+++ Assets/Scripts/EnnemyScript.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class EnnemyScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private Rigidbody2D body;
+    [SerializeField]
+    private Transform initialPosition;
+    [SerializeField]
+    private int direction = 1;
+    [SerializeField]
+    private float maxDist;
+    [SerializeField]
+    private float minDist;
+    [SerializeField]
+    private float movingSpeed;
+
+   
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+        //initialPosition = transform.position;
+        direction = -1;
+        maxDist += transform.position.x;
+        minDist -= transform.position.x;
+        
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        switch (direction)
+        {
+            case -1:
+                //Move Left
+                if(transform.position.x > minDist)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(-movingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                }
+                else
+                {
+                    direction = 1;
+                }
+                break;
+            case 1:
+                //Move Right
+                if(transform.position.x < maxDist)
+                {
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(movingSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                }
+                else
+                {
+                    direction = -1;
+                }
+                break;
+
+                
+        }
+        
+    }
+   
 }
