51f61cf75d1ef9091a659a310ed450307587ede3
DKCoffee <amk1295@gmail.com>
2017-11-24 14:29:43 +0100

    Add Enemy Spawn

---
+++ Assets/Scripts/EnemySpawnScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnScript : MonoBehaviour {
+
+    [SerializeField] private GameObject EnemyPrefab;
+    [SerializeField] private Transform SpawnTransform;
+    [SerializeField] private float TimeToSpawn;
+    [SerializeField] private float LasttimeSpawn;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", TimeToSpawn, TimeToSpawn);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void Spawn()
+    {
+        if(Time.realtimeSinceStartup - LasttimeSpawn > TimeToSpawn)
+        {
+            Instantiate(EnemyPrefab, SpawnTransform.position, SpawnTransform.rotation);
+        }
+    }
+}
