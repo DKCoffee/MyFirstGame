377660e014f4810440e2296dda8ab64c6f8a1a37
DKCoffee <amk1295@gmail.com>
2017-11-26 19:45:08 +0100

    working on save score and highscore

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,27 +7,33 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     private int score;
+    [SerializeField] private Text scoreSave;
+    [SerializeField] private Text highscore;
 
-    [SerializeField]
-    private Text textScore;
-
-    private const string TEXT_SCORE = "Score : ";
 
     // Use this for initialization
     void Start()
     {
-       
+        scoreSave.text = PlayerPrefs.GetInt("ScoreSave", 0).ToString();
+        highscore.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+
     }
 
     public void AddScore()
     {
         score++;
-        textScore.text = "Score: " + score;
+        scoreSave.text = score.ToString();
+
+        if(score > PlayerPrefs.GetInt("Highscore",0))
+        {
+            PlayerPrefs.GetInt("Highscore", score);
+            highscore.text = score.ToString();
+        }
         if(score >= 30)
         {
             SceneManager.LoadScene("Victory");
