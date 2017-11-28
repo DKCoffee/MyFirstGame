50d2f613eb4478019620828b78986e6929a5d647
DKCoffee <amk1295@gmail.com>
2017-11-28 18:25:57 +0100

    Add defeat sound and small corrections

---
+++ Assets/Scripts/ButtonScripts/CreditButtonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditButtonController : MonoBehaviour
+
+{
+    [SerializeField]
+    public Button creditButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button button = creditButton.GetComponent<Button>();
+        button.onClick.AddListener(LoadLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
